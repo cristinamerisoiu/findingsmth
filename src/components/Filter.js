519535ca3d577e76f7d44b1d9598a83b9976276a
Cristina Merisoiu <cristina.merisoiu@gmail.com>
2019-09-11 12:10:02 +0200

    add placeholder components

---
+++ src/components/Filter.js
@@ -0,0 +1,6 @@
+import React from "react";
+function Filter() {
+  return <div>Filter</div>;
+}
+
+export default Filter;
---
+++ src/components/Markets.js
@@ -0,0 +1,6 @@
+import React from "react";
+function Markets() {
+  return <div>Markets</div>;
+}
+
+export default Markets;
