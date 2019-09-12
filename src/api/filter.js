35cf05e4e71f30c769cc15c97bf4f997e71e1523
Cristina Merisoiu <cristina.merisoiu@gmail.com>
2019-09-12 15:04:14 +0200

    add filter array with options

---
+++ src/api/filter.js
@@ -0,0 +1,14 @@
+export const filter = [
+  {
+    name: "distance",
+    options: ["<2min", "<3min", "<10min"]
+  },
+  {
+    name: "category",
+    options: ["salads", "healthy", "bio"]
+  },
+  {
+    name: "rating",
+    options: ["****", "***", "**"]
+  }
+];
--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,6 +1,13 @@
 import React from "react";
-function Filter() {
-  return <section className="filter">Filter</section>;
+function Filter(props) {
+  return (
+    <select className="filter">
+      <option className="badges">{props.filter.name}</option>
+      <option className="badges">{props.filter.options}</option>
+      <option className="badges">{props.filter.options}</option>
+      <option className="badges">{props.filter.options}</option>
+    </select>
+  );
 }
 
 export default Filter;
