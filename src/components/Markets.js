de6ffcd44b1322338f68963fa24534994f158dcb
Cristina Merisoiu <cristina.merisoiu@gmail.com>
2019-09-14 18:29:59 +0200

    add imgs and content in the market cards

--- src/components/Markets.js
+++ src/components/Markets.js
@@ -2,9 +2,28 @@ import React from "react";
 function Markets(props) {
   return (
     <div className="market">
-      {props.markets.title}
-      <img src={props.markets.imgSrc} alt={props.markets.title} />
-      <p>{props.markets.description}</p>
+      <p className="market__title">{props.markets.title}</p>
+      <img
+        src={props.markets.imgSrc}
+        className="img__market"
+        alt={props.markets.title}
+      />
+      <p className="market__description">{props.markets.description}</p>
+      <p className="market__prop">
+        {" "}
+        Distance: {props.markets.distance} minutes
+        <br />
+        Rating: {props.markets.rating} stars
+        <br />
+        Categories:{""}
+        {props.markets.categories.map(category => {
+          return (
+            <span className="card__category" key={category}>
+              {category}
+            </span>
+          );
+        })}
+      </p>
     </div>
   );
 }
