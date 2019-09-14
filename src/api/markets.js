2655eed4e45a0b2b56030587dcc0dc73cb8082f3
Cristina Merisoiu <cristina.merisoiu@gmail.com>
2019-09-14 23:17:20 +0200

    add meda queries

--- src/api/markets.js
+++ src/api/markets.js
@@ -4,16 +4,16 @@ export const markets = [
       "https://images.unsplash.com/photo-1501523460185-2aa5d2a0f981?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
     title: "Tanzbrunnen",
     categories: ["seafood", "fish"],
-    distance: 1,
-    rating: 4.1,
-    description: "Best for fresh fish"
+    distance: 2,
+    rating: 3.9,
+    description: "Best for fresh fish and sushi"
   },
   {
     imgSrc:
       "https://images.unsplash.com/photo-1543083477-4f785aeafaa9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
     title: "Wilhemplatz",
     categories: ["fruits", "vegetables", "cheese"],
-    distance: 4,
+    distance: 6,
     rating: 4.8,
     description: "Fruits and vegetables"
   },
@@ -22,7 +22,7 @@ export const markets = [
       "https://images.unsplash.com/photo-1547977428-f3436426337a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
     title: "Rudolplatz",
     categories: ["organic", "bio", "healthy"],
-    distance: 1,
+    distance: 3,
     rating: 4.2,
     description: "The organic food market"
   }
