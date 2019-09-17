4ce124dd8ba395a8503849bf30efe527b42f5afb
Cristina Merisoiu <cristina.merisoiu@gmail.com>
2019-09-17 17:38:24 +0200

    add value to the selected filters

--- src/api/markets.js
+++ src/api/markets.js
@@ -4,7 +4,7 @@ export const markets = [
       "https://images.unsplash.com/photo-1501523460185-2aa5d2a0f981?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
     title: "Tanzbrunnen",
     categories: ["seafood", "fish"],
-    distance: 2,
+    distance: 1,
     rating: 3.0,
     description: "Best for fresh fish and sushi"
   },
--- src/components/MarketsList.js
+++ src/components/MarketsList.js
@@ -5,47 +5,55 @@ import { markets } from "../api/markets";
 function MarketsList({ selectedFilters }) {
   console.log(selectedFilters);
   const filteredMarkets = markets.filter(markets => {
-    if (!selectedFilters) {
-      return true;
-    }
+    let keepMarkets = true;
     if (selectedFilters.distance) {
       switch (selectedFilters.distance) {
-        case "< 2min":
-          return markets.distance < 2;
-        case "< 3min":
-          return markets.distance < 3;
-        case "< 10min":
-          return markets.distance < 10;
+        case "<2min":
+          keepMarkets = markets.distance < 2;
+          break;
+        case "<3min":
+          keepMarkets = markets.distance < 3;
+          break;
+        case "<10min":
+          keepMarkets = markets.distance < 10;
+          break;
         default:
           break;
       }
     }
-    if (selectedFilters.categories) {
-      switch (selectedFilters.categories) {
-        case "salads":
-          return markets.categories === "salads";
-        case "healthy":
-          return markets.categories === "healthy";
-        case "bio":
-          return markets.categories === "bio";
-        default:
-          break;
-      }
-      return markets.categories.includes(selectedFilters.categories);
+    if (keepMarkets && selectedFilters.rating) {
+      keepMarkets = markets.rating = selectedFilters.rating.lenght;
     }
-    if (selectedFilters.name === "rating") {
-      switch (selectedFilters.value) {
-        case "**":
-          return markets.rating > 2;
-        case "***":
-          return markets.rating > 3;
-        case "****":
-          return markets.rating > 4;
-        default:
-          break;
-      }
+    if (keepMarkets && selectedFilters.category) {
+      keepMarkets = markets.categories.includes(selectedFilters.category);
     }
-    return true;
+    return keepMarkets;
+    // if (selectedFilters.categories) {
+    //   switch (selectedFilters.categories) {
+    //     case "salads":
+    //       return markets.categories === "salads";
+    //     case "healthy":
+    //       return markets.categories === "healthy";
+    //     case "bio":
+    //       return markets.categories === "bio";
+    //     default:
+    //       break;
+    //   }
+    //   return markets.categories.includes(selectedFilters.categories);
+    // }
+    // if (selectedFilters.name === "rating") {
+    //   switch (selectedFilters.value) {
+    //     case "**":
+    //       return markets.rating > 2;
+    //     case "***":
+    //       return markets.rating > 3;
+    //     case "****":
+    //       return markets.rating > 4;
+    //     default:
+    //       break;
+    //   }
+    // }
+    // return true;
   });
 
   return (
