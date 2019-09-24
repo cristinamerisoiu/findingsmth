fca002bca42e25eac3d0839205d7f51ae33eb869
Cristina Merisoiu <cristina.merisoiu@gmail.com>
2019-09-24 11:09:17 +0200

    add url search and route path

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "./App.css";
-import FilterList from "./components/FilterList";
-import Title from "./components/Header";
 // import Markets from "./components/Markets";
-import MarketsList from "./components/MarketsList";
 import styled from "styled-components";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import New from "./pages/New";
 
 const StyledApp = styled.div`
   background: lightblue;
@@ -12,51 +12,24 @@ const StyledApp = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  width: 100vw;
-  height: 100vh;
+  height: 100%;
 `;
 
-const Main = styled.main`
-  flex-grow: 1;
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
+//const newFilter = {
+//name: name,
+//value: value
+//};
 
+// or Object.assign({}, filters)
+//setFilters(newFilter);
 function App() {
-  const [filters, setFilters] = React.useState({
-    distance: "< 1min"
-  });
-
-  function handleFilterChange(name, value) {
-    const newFilters = { ...filters };
-    if (value) {
-      newFilters[name] = value;
-    } else {
-      delete newFilters[name];
-    }
-    setFilters(newFilters);
-  }
-  //const newFilter = {
-  //name: name,
-  //value: value
-  //};
-
-  // or Object.assign({}, filters)
-  //setFilters(newFilter);
-
   return (
     <>
       <StyledApp>
-        <Title />
-        <Main>
-          <FilterList
-            selectedFilters={filters}
-            onFilterChange={handleFilterChange}
-          />
-          <MarketsList selectedFilters={filters} />
-        </Main>
+        <Router>
+          <Route path="/" exact component={Home} />
+          <Route path="/new" exact component={New} />
+        </Router>
       </StyledApp>
     </>
   );
